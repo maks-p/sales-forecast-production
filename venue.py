7ac973c425efe94b15bf9a6dfcaeb6f4fa2296cc
Maks Pazuniak <mpazuniak@gmail.com>
2019-09-06 14:27:16 -0400

    Cleaned up Venue Class

--- venue.py
+++ venue.py
@@ -6,16 +6,14 @@ class Venue:
     def __init__(self, name, location):
         self.name = name
         self.location = location
-        self.lat, self.long = Location(name, location).lat_long()
+        self.loc = Location(name, location)
     
     def weather_dataframe(self, start_date, end_date):
-        return Weather(self.lat, self.long).weather_df(start_date, end_date)
+        lat, long = venue.loc.lat_long()
+        return Weather(lat, long).weather_df(start_date, end_date)
 
     def daily_weather(self, day):
         return Weather(self.lat, self.long).daily_weather(day)
 
-    def yelp_rating(self):
-        return Location(self.name, self.location).yelp_rating()
-
 venue = Venue('Jupiter Disco', 'Brooklyn, NY')
-print(venue.Location)
+print(venue.loc.yelp_rating())
